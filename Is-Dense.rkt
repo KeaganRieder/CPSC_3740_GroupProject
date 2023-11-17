a20f1e1fcb25e3f811b60f7b81e4bd9593233457
JustinWolfenden <jwolf4@hotmail.com>
2023-11-17 13:27:56 -0700

    done coeff
    coeff works well, however, if the user tells the program to search for something with a negative power, it will break

--- Is-Dense.rkt
+++ Is-Dense.rkt
@@ -19,3 +19,18 @@
          ;its not add 0 to the list and look for the next value
          (append (list 0) (to-dense x (+ 1 y))))))
 
+; checks if its dense then makes it dense if it is not already dense
+(define (make-dense x)
+  (if (is-dense? x)
+      x
+      (to-dense x 0)))
+
+; returns the coefficent of x^y converts all lists to dense for ease of implementation negative y will break this
+(define (coeff x y)
+  ;round about way of asking is y less then or equal to length x
+  (if (negative? (- y (length x)))
+              (list-ref (make-dense x) y)
+              ;returns false if y will not work
+              false))
+
+(coeff (list (list 1 0) (list 2 1) (list 3 2) (list 4 3)) 4)
